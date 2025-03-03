6722578e390bb88613195d95aaa29b121eaea6b3
cuisonenrico <cuisonenrico@gmail.com>
2025-03-04 01:21:53 +0800

    Initial UI and API integration
    - Added movieDetails, movieHome, and profile pages (profile does not have anything yet)
    - movieHome fetches and displays 10 posters for now. Will add search functionality and maybe a load next page on scroll end.
    - movieDetails show more details about the movie. Fetches details from API using imdbId from the prior list of movies. UI improvements for this is necessary (buttons and Image enhancements) **Image for actors/directors/writers not available with API
    - Used AsyncStorage to manage the state for selecting and loading movie details

---
+++ app/movieDetails.tsx
@@ -0,0 +1,214 @@
+import {
+  ActivityIndicator,
+  ScrollView,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { useTheme } from "../context/themeContext";
+import AppText from "./components/app_text";
+import AppSafeAreaView from "./components/app_safe_area_view";
+import AppStatusBar from "./components/app_status_bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { Movie } from "@/models/movie";
+import { MovieDetailed } from "@/models/movie_detailed";
+
+const movieDetails = () => {
+  const { theme } = useTheme();
+
+  var isDarkTheme = theme === "dark";
+  const screenSize = Dimensions.get("window");
+
+  const [movie, setMovie] = useState<Movie>();
+  const [movieDetailed, setMovieDetailed] = useState<MovieDetailed>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadMovie = async () => {
+      const storedMovie = await AsyncStorage.getItem("selectedMovie");
+
+      if (storedMovie) {
+        var movieObj = JSON.parse(storedMovie);
+        setMovie(movieObj);
+        try {
+          const response = await axios.get(
+            `https://www.omdbapi.com/?apikey=b9bd48a6&i=${movieObj["imdbID"]}&plot=full`
+          );
+
+          setMovieDetailed(response.data);
+        } catch (err) {
+          setError(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+      setLoading(false);
+    };
+
+    loadMovie();
+  }, []);
+
+  if (loading)
+    return (
+      <ActivityIndicator
+        className={`${
+          isDarkTheme ? "bg-dark" : "bg-white"
+        }flex-1 items-center justify-center`}
+        size="large"
+        color="blue"
+      />
+    );
+  if (!movie || !movieDetailed) return <Text>No movie found</Text>;
+
+  return (
+    <AppSafeAreaView isDarkTheme={isDarkTheme}>
+      <AppStatusBar isDarkTheme={isDarkTheme} />
+      <ScrollView
+        className={`${isDarkTheme ? "bg-dark" : "bg-white"}`}
+        style={{ width: screenSize.width, height: screenSize.height }}
+      >
+        <View className="relative w-full h-700">
+          <TouchableOpacity activeOpacity={0.8}>
+            <Image
+              resizeMethod="scale"
+              source={{ uri: movieDetailed.Poster }}
+              className=""
+              style={{ width: screenSize.width, height: 500 }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View className="relative -top-14 bottom-0 left-0 right-0 bg-transparent flex-row items-center p-4">
+          <Image
+            source={{ uri: movieDetailed.Poster }} // Replace with the proper image URL
+            className="w-20 h-30 rounded-lg"
+            style={{ width: 120, height: 180, marginVertical: 0 }}
+          />
+          <View className="relative mt-12 ml-4 bottom-0">
+            <AppText
+              text={movieDetailed.Title}
+              isDarkTheme={isDarkTheme}
+              customStyle="flex-wrap-reverse flex text-2xl font-bold mr-12"
+            />
+
+            <AppText
+              text={movieDetailed.Year}
+              isDarkTheme={isDarkTheme}
+              customStyle="text-gray-500"
+            />
+            <AppText
+              text={`${movieDetailed.Runtime} | ${movieDetailed.Genre}`}
+              isDarkTheme={isDarkTheme}
+              customStyle="text-gray-700 flex-wrap-reverse flex"
+            />
+
+            <Text className="text-green-600">
+              {movieDetailed.imdbRating} ⭐ ({movieDetailed.imdbVotes} Reviews)
+            </Text>
+          </View>
+        </View>
+        <View className="relative -top-16 bottom-0 left-0 right-0 p-4">
+          <AppText
+            text="Plot Summary"
+            isDarkTheme={isDarkTheme}
+            customStyle="text-lg font-semibold mb-2"
+          />
+          <AppText
+            text={movieDetailed.Plot}
+            isDarkTheme={isDarkTheme}
+            customStyle="text-gray-800 mb-4"
+          />
+          <AppText
+            text="Cast Overview"
+            isDarkTheme={isDarkTheme}
+            customStyle="text-lg font-semibold mb-2"
+          />
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {movieDetailed.Actors.split(",").map((actor, index) => (
+              <View key={index} className="mr-4">
+                <Image
+                  source={{ uri: `https://.../${actor}` }} // Replace with the proper image URL
+                  className="w-16 h-24 rounded-lg"
+                />
+                <AppText
+                  text={actor}
+                  isDarkTheme={isDarkTheme}
+                  customStyle="text-center text-sm"
+                />
+                <AppText
+                  text="Actor/Actress"
+                  isDarkTheme={isDarkTheme}
+                  customStyle="text-center text-sm text-gray-400"
+                />
+              </View>
+            ))}
+            {movieDetailed.Director.split(",").map((director, index) => (
+              <View key={index} className="mr-4">
+                <Image
+                  source={{ uri: `https://.../${director}` }} // Replace with the proper image URL
+                  className="w-16 h-24 rounded-lg"
+                />
+                <AppText
+                  text={director}
+                  isDarkTheme={isDarkTheme}
+                  customStyle="text-center text-sm"
+                />
+                <AppText
+                  text="Director"
+                  isDarkTheme={isDarkTheme}
+                  customStyle="text-center text-sm text-gray-400"
+                />
+              </View>
+            ))}
+            {movieDetailed.Writer.split(",").map((writer, index) => (
+              <View key={index} className="mr-4">
+                <Image
+                  source={{ uri: `https://.../${writer}` }} // Replace with the proper image URL
+                  className="w-16 h-24 rounded-lg"
+                />
+                <AppText
+                  text={writer}
+                  isDarkTheme={isDarkTheme}
+                  customStyle="text-center text-sm"
+                />
+                <AppText
+                  text="Writer"
+                  isDarkTheme={isDarkTheme}
+                  customStyle="text-center text-sm text-gray-400"
+                />
+              </View>
+            ))}
+          </ScrollView>
+          {/* User Reviews */}
+          <AppText
+            text="User Reviews (from IMDB)"
+            isDarkTheme={isDarkTheme}
+            customStyle="text-lg font-semibold mb-2"
+          />
+          <AppText
+            text="Justice League is, unfortunately, plagued with post-production issues..."
+            isDarkTheme={isDarkTheme}
+            customStyle="text-gray-800 mb-4"
+          />
+          <View className="flex-row justify-between w-full max-w-xs mb-24">
+            <TouchableOpacity className="flex-1 items-center">
+              <Text className="text-gray-500">LIKE</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-1 items-center">
+              <Text className="text-gray-500">FAVORITE</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-1 items-center">
+              <Text className="text-gray-500">SHARE</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </AppSafeAreaView>
+  );
+};
+
+export default movieDetails;
